ef2f368cf8081ccaf5d49b3ae0ebcd38d8a8586e
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 20:20:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+
+  attr_reader :species
+  attr_accessor :name, :pets, :fish, :dog, :cat
+
+  @@all = []
+  @@ pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+
+  def initialize(species)
+    @species = species
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
 end
