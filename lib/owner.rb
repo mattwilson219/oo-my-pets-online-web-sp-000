98da5ba109b719d624af25bb70a4f67934074516
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 20:32:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,10 +67,7 @@ class Owner
   end
 
   def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    num_fish = @pets[:fishes].size
-    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 
 end
