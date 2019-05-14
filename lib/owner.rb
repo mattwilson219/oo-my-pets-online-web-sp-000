3c1a00e2b5dc405b80d27769c3913aced6ded039
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 20:22:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,4 +28,32 @@ class Owner
     "I am a #{species}."
   end
 
+  def pets
+    @@pets
+  end
+
+  def buy_fish(fish)
+    @@pets[:fishes] << Fish.new(fish)
+  end
+
+  def buy_cat(cat)
+    @@pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog)
+  end
+
+  def walk_dogs
+    @@pets[:dogs].collect {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @@pets[:cats].collect {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @@pets[:fishes].collect {|fish| fish.mood = "happy"}
+  end
+
 end
